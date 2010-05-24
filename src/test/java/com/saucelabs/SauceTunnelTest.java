c7da5d42ba559e0d94c7ebfb5085ecf974d9b0d2
Kohsuke Kawaguchi <kk@kohsuke.org>
2010-05-24 14:47:52 -0700

    test that puts everything together

--- src/test/java/com/saucelabs/SauceTunnelTest.java
+++ src/test/java/com/saucelabs/SauceTunnelTest.java
@@ -23,9 +23,30 @@
  */
 package com.saucelabs;
 
+import com.saucelabs.rest.Credential;
 import com.saucelabs.rest.SauceTunnel;
 import com.saucelabs.rest.SauceTunnelFactory;
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.Selenium;
 import junit.framework.TestCase;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.bio.SocketConnector;
+import org.mortbay.jetty.handler.ResourceHandler;
+import org.mortbay.jetty.servlet.ServletHandler;
+import org.mortbay.jetty.servlet.ServletHolder;
+import org.mortbay.jetty.servlet.ServletMapping;
+import org.mortbay.jetty.webapp.Configuration;
+import org.mortbay.jetty.webapp.WebAppContext;
+import org.mortbay.jetty.webapp.WebXmlConfiguration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
 
 /**
  * Unit test for {@link SauceTunnel}.
@@ -49,6 +70,63 @@ public class SauceTunnelTest extends TestCase
             System.out.println("shutdown="+t.getShutDownTime());
             System.out.println("running="+t.isRunning());
             System.out.println();
+        }
+    }
+
+    /**
+     * Start a web server locally, set up an SSH tunnel, and have Sauce OnDemand connect to the local server.
+     */
+    public void testFullRun() throws Exception {
+        final int code = new Random().nextInt();
+
+        // start the Jetty locally and have it respond our secret code.
+        Server server = new Server();
+        ServletHandler handler = new ServletHandler();
+        handler.addServletWithMapping(new ServletHolder(new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                resp.setContentType("text/html");
+                resp.getWriter().println("<html><head><title>test"+code+"</title></head><body>it works</body></html>");
+            }
+        }),"/");
+        server.setHandler(handler);
+
+        SocketConnector connector = new SocketConnector();
+        server.addConnector(connector);
+        server.start();
+        System.out.println("Started Jetty at "+connector.getLocalPort());
+
+        try {
+            // start a tunnel
+            System.out.println("Starting a tunnel");
+            SauceTunnelFactory tunnelFactory = new SauceTunnelFactory();
+            SauceTunnel t = tunnelFactory.create("test"+code+".org");
+            t.waitUntilRunning(60000);
+            t.connect(80,"localhost",connector.getLocalPort());
+            System.out.println("tunnel established");
+
+
+            try {
+                Credential c = new Credential();
+                DefaultSelenium selenium = new DefaultSelenium(
+                            "saucelabs.com",
+                            4444,
+                            "{\"username\": \""+c.getUsername()+"\"," +
+                            "\"access-key\": \""+c.getKey()+"\"," +
+                            "\"os\": \"Windows 2003\"," +
+                            "\"browser\": \"firefox\"," +
+                            "\"browser-version\": \"3.\"," +
+                            "\"job-name\": \"This is an example test\"}",
+                            "http://test"+code+".org/");
+                selenium.start();
+                selenium.open("/");
+                assertEquals("test"+code,selenium.getTitle());
+                selenium.stop();
+            } finally {
+                t.destroy();
+            }
+        } finally {
+            server.stop();
         }
     }
 }
