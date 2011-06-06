1f21c699eaed541e1f476bcd663a471d0a62a229
Ross Rowe <piaross@gmail.com>
2011-06-06 16:45:42 +1000

    Changed build number to be a String

--- src/main/java/com/saucelabs/rest/UpdateJob.java
+++ src/main/java/com/saucelabs/rest/UpdateJob.java
@@ -10,11 +10,11 @@ public class UpdateJob {
     public String name;
     @JsonProperty("public") public boolean _public;
     public List<String> tags;
-    public int build;
+    public String build;
     public boolean passed;
     @JsonProperty("custom-data") public Map<String, Object> customData;
 
-    public UpdateJob(String name, boolean _public, List<String> tags, int build, boolean passed, Map<String, Object> customData) {
+    public UpdateJob(String name, boolean _public, List<String> tags, String build, boolean passed, Map<String, Object> customData) {
         this.name = name;
         this._public = _public;
         this.tags = tags;
